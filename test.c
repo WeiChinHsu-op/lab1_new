6a26ba75f848bcbaad76097ea14df762d800b1db
WeiChinHsu-op <eric87092611@gmail.com>
2020-02-29 23:18:47 +0800

    Add helloworld.py

---
+++ test.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ 2 
+ 3 int main (void) {
+ 4     int i;
+ 5     // print hello world for 9 times
+ 6     for (i=1; i<5; ++i) {
+ 7         printf("hello world %d times!\n", i);
+ 8     }
+ 9     return 0;
+10 }
